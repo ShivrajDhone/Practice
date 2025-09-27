4d4977c7360608c55c0370efc95e1624f71bb332
ShivrajDhone <shivrajdhone777@gmail.com>
2025-09-27 21:44:12 +0530

    Updated Page1 with Bootstrap form second time

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h2>Shivraj</h2>
+      <h2>Anand</h2>
+      <h2>Dhone</h2>
     </>
   )
 }
